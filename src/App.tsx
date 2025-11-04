a99de85cae5437e15fd8cc51b78a8a61935c1dbb
Fernando <fernando.bernardes@portosecosuldeminas.com.br>
2025-11-03 21:34:04 -0300

    Fix: Adicionar basename no BrowserRouter para funcionar com GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gcti">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
